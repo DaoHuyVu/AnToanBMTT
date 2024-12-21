0ae94f9cd8d56dd87d7d4aea3aea4ae14801f48c
Dao Huy Vu <huyvu160102@gmail.com>
2024-12-22 01:07:57 +0700

    Add logic to navigate to login or main screen

--- app/src/main/java/com/example/antoanbmtt/SplashActivity.kt
+++ app/src/main/java/com/example/antoanbmtt/SplashActivity.kt
@@ -8,19 +8,28 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import androidx.lifecycle.lifecycleScope
+import com.example.antoanbmtt.repository.UserDataStore
 import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.first
 import kotlinx.coroutines.launch
+import javax.inject.Inject
 
 @SuppressLint("CustomSplashScreen")
 @AndroidEntryPoint
 class SplashActivity : AppCompatActivity() {
+    @Inject private lateinit var dataStore : UserDataStore
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
         lifecycleScope.launch {
             delay(1000)
-            startActivity(Intent(this@SplashActivity,LoginActivity::class.java))
+            val email = dataStore.emailFlow.first()
+            if(email.isNotEmpty()){
+                startActivity(Intent(this@SplashActivity,MainActivity::class.java))
+            }
+            else
+                startActivity(Intent(this@SplashActivity,LoginActivity::class.java))
             finish()
         }
     }
